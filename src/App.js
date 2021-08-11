9a9535ca2cf450315a598bc28a88577d4bbe9768
Aditya <studaditya100100@gmail.com>
2021-08-11 20:01:05 +0530

    ReFormatted Pages and Components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 //Complementary-Functions
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //Screen-Imports
-import { HomeScreen } from './screens/HomeScreen';
+import { HomeScreen } from "./screens/HomeScreen";
 function App() {
   return (
     <>
       <Router>
-
-
         <Route component={HomeScreen} exactpath="/" />
-
-
-
       </Router>
     </>
-
   );
 }
 
