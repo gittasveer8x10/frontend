3dbb89a8558398e4e9630b28e73a31fb54981942
Aman Yadav <aman.ydv.2112@gmail.com>
2021-08-20 19:31:56 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router,Route  } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import RegisterScreen from './screens/RegisterScreen'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import RegisterScreen from "./screens/RegisterScreen";
 
 function App() {
   return (
     <Router>
-      <main className="py-3">
-        <Container>
-        <Route path='/' component={RegisterScreen} exact/>
-        </Container>
-      </main>
-  </Router>
+      <Route path="/register" component={RegisterScreen} exact />
+    </Router>
   );
 }
 
--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,63 +1,86 @@
-import React from 'react'
-import { Form,Button,Row,Col,Container,Image} from 'react-bootstrap'
-import login from '../images/login.png'
-import '../css/RegisterScreen.css'
-import google from '../images/google.png'
-import facebook from '../images/facebook.png'
-import linkedin from '../images/linkedin.png'
-import call from '../images/call.png'
-import key from '../images/key.png'
-import padlock from '../images/padlock.png'
-import user from '../images/user.png'
-import email from '../images/email.png'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/RegisterScreen.css";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  CloseButton,
+} from "react-bootstrap";
 function RegisterScreen() {
-    return (
-    <div id="login-box">
-
-        <div class="left">
-          <h1>Register</h1>
-          <hr/>
-          <div class="box-2">
-            <img class="social_sites2" src={user} alt="user" width="13px" height="13px"/>
-            <input type="text" name="username" placeholder="*Full Name" />
-          </div>
-
-          <div class="box-2"> 
-            <img class="social_sites2" src={call} alt="call" width="13px" height="13px"/>
-            <input type="text" name="contact" placeholder="*Contact Number" />
-          </div>
-
-          <div class="box-2">
-            <img class="social_sites2" src={email} alt="email" width="13px" height="13px"/>
-            <input type="text" name="email" placeholder="*Email" />
-          </div>
-
-          <div class="box-2">
-            <img class="social_sites2" src={key} alt="key" width="13px" height="13px"/>
-             <input type="password" name="password1" placeholder="*Create Password" />
-          </div>
-
-          <div class="box-2">
-            <img class="social_sites2" src={padlock} alt="padlock" width="13px" height="13px"/>
-            <input type="password" name="password2" placeholder="*Confirm password" />
-          </div>
-          
-          <input type="submit" name="signup_submit" value="Register" />
-
-          <div class="login-bottom">Already have an account?<a href='/login'>Login</a></div>
-          <div class="login-bottom">Or Login with
-          <img class="social_sites" src={google} alt="google" width="13px" height="13px"/>
-          <img class="social_sites" src={facebook} alt="facebook" width="13px" height="13px"/>
-          <img class="social_sites" src={linkedin} alt="linkedin" width="13px" height="13px"/>
-          </div>
-        </div>
-
-        <div class="right"></div>
-
-    </div>
-        
-    )
+  return (
+    <Container id="login-box">
+      <CloseButton className="my-2" />
+      <Col id="left">
+        <h2 className="font-weight-bold">Register</h2>
+        <hr style={{ marginBottom: "6px" }} />
+        <Link id="uniqueId">#ASDFIG</Link>
+        <Form>
+          <Row>
+            <i className="fas fa-user"></i>
+            <Form.Group controlId="name" style={{ width: "20px" }}>
+              <Form.Control type="text" placeholder="*Full name"></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <i className="fas fa-phone-alt"></i>
+            <Form.Group controlId="contact" style={{ width: "20px" }}>
+              <Form.Control
+                type="text"
+                placeholder="*Contact Number"
+              ></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <i className="fas fa-envelope"></i>
+            <Form.Group controlId="email" style={{ width: "20px" }}>
+              <Form.Control type="email" placeholder="*Email"></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <i className="fas fa-key"></i>
+            <Form.Group controlId="password" style={{ width: "20px" }}>
+              <Form.Control
+                type="password"
+                placeholder="*Create Password"
+              ></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <i className="fas fa-lock"></i>
+            <Form.Group controlId="password" style={{ width: "20px" }}>
+              <Form.Control
+                type="password"
+                placeholder="*Confirm Password"
+              ></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row md={2} className="justify-content-center">
+            <Button type="submit" variant="dark">
+              Register
+            </Button>
+          </Row>
+        </Form>
+        <Row className="justify-content-center my-2">
+          <h6 style={{ width: "14rem", fontSize: "12px", fontWeight: "bold" }}>
+            Already have an account?
+            <Link style={{ color: "orange" }}>Login</Link>
+          </h6>
+        </Row>
+        <Row className="justify-content-center">
+          <h6 style={{ width: "12rem", fontSize: "12px", fontWeight: "bold" }}>
+            Or Login with:
+            <i className="fab fa-google mx-2"></i>
+            <i className="fab fa-facebook mx-2"></i>
+            <i className="fab fa-linkedin mx-2"></i>
+          </h6>
+        </Row>
+      </Col>
+      <Col id="right"></Col>
+    </Container>
+  );
 }
 
-export default RegisterScreen
+export default RegisterScreen;
